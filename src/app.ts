6784e1e3635cf8d5275bc0044d205d5b4e978700
Ashik <asikhosen865575@gmail.com>
2025-07-19 15:19:29 +0600

    setup

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import  express, { Application, Request, Response }  from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser"
+import router from "./app/routes";
+
+
+const app:Application= express();
+app.use(cors());
+app.use(express.json());
+// app.use(express.urlencoded({extended:true}));
+// app.use(cookieParser());
+
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send({
+        message:"Marriage document server is running......"
+    })
+
+})
+
+// app.use("/api/v1/user",userRoutes)
+// app.use("/api/v1/admin",AdminRoutes)
+
+app.use("/api/v1",router);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import { Server } from "http";
+import app from "./app";
+import config from "./config";
+
+async function main(){
+    const server:Server= app.listen(config.port,()=>{
+        console.log(`Server is running on port ${config.port}`);
+
+    })
+
+
+    process.on('uncaughtException',(error)=>{
+        console.log(error)
+        if(server){
+            server.close(()=>{
+                console.info("Server close")
+            })
+        }
+    })
+    
+
+    process.on('unhandledRejection',(error)=>{
+            console.log(error)
+            if(server){
+                server.close(()=>{
+                    console.info("Server close")
+                })
+            }
+    })
+
+
+}
+
+main();
